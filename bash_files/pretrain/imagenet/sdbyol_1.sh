da1717e4301296b2fbbb8b6c98f662c4d8e78a75
samuel.lavoie <samuel.lavoie.m@gmail.com>
2022-01-04 10:04:18 -0500

    byol bash file

---
+++ bash_files/pretrain/imagenet/sdbyol_1.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+ROOT_PATH=$1
+
+../../../prepare_data.sh TEST
+
+source ~/env/bin/activate
+
+python3 ../../../main_pretrain.py \
+    --dataset imagenet \
+    --backbone resnet50 \
+    --checkpoint_dir="$ROOT_PATH" \
+    --data_dir $SLURM_TMPDIR/data \
+    --train_dir $SLURM_TMPDIR/data/train/ \
+    --val_dir   $SLURM_TMPDIR/data/val/ \
+    --max_epochs 512 \
+    --gpus 0,1,2,3 \
+    --accelerator gpu \
+    --strategy ddp \
+    --sync_batchnorm \
+    --precision 16 \
+    --optimizer sgd \
+    --lars \
+    --eta_lars 0.001 \
+    --exclude_bias_n_norm \
+    --scheduler warmup_cosine \
+    --lr 0.45 \
+    --accumulate_grad_batches 16 \
+    --classifier_lr 0.2 \
+    --weight_decay 1e-6 \
+    --batch_size 128 \
+    --num_workers 4 \
+    --dali \
+    --brightness 0.4 \
+    --contrast 0.4 \
+    --saturation 0.2 \
+    --hue 0.1 \
+    --gaussian_prob 1.0 0.1 \
+    --solarization_prob 0.0 0.2 \
+    --num_crops_per_aug 1 1 \
+    --name "orion_sdbyol-FULL" \
+    --entity il_group \
+    --project VIL \
+    --wandb \
+    --offline \
+    --save_checkpoint \
+    --method sdbyol \
+    --proj_output_dim 256 \
+    --proj_hidden_dim 4096 \
+    --pred_hidden_dim 4096 \
+    --base_tau_momentum 0.99 \
+    --final_tau_momentum 1.0 \
+    --voc_size 14 \
+    --message_size 410 \
+    --tau_online 2.1 \
+    --tau_target 1.8 \
+    --momentum_classifier
